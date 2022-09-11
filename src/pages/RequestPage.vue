bcf9bae17ca2076ab1c96b7b7fc3c46d9a17024f
Soobratty Anas <soobratty.as@gmail.com>
2022-09-11 22:07:13 +0400

    Adding matching route components for all pages

---
+++ src/pages/RequestPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>From Request Page</h1>
+</template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import coachesPage from './pages/CoachesPage.vue'
 import SingleCoach from './pages/SingleCoach.vue'
 import SingleCoachContact from './pages/SingleCoachContact.vue'
+import RegisterPage from './pages/RegisterPage.vue'
+import RequestPage from './pages/RequestPage.vue'
+import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,12 +13,12 @@ const router = createRouter({
         { path: '/coaches', component: coachesPage },
         {
             path: '/coaches/:id', component: SingleCoach, children: [
-                { path: '/contact', component: SingleCoachContact }
+                { path: 'contact', component: SingleCoachContact }
             ]
         },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null },
+        { path: '/register', component: RegisterPage },
+        { path: '/requests', component: RequestPage },
+        { path: '/:notFound(.*)', component: NotFound },
     ]
 })
 
